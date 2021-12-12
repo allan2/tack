662c2e72effa430161a01e4f5cb55fdca14bebed
allan2 <allanzhang7@gmail.com>
2021-12-11 21:25:08 -0330

    Add markup of entities

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # tack
+
+A syntax highlighter that outputs HTML.
+
+My goal was to create an HTML highlighter that outputs minimal markup.
+
+- class-based
+- puntuation is not marked up
+
--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,31 @@ fn main() {
 		display: flex;
 		flex-direction: column;
 		min-height: 100%;
-	}";
+	}"
+    .to_string();
 
-    valid_braces(code_css);
+    assert!(valid_braces(&code_css));
+    println!("{}", html_gen(code_css));
 }
 
-fn html_gen(code_css: String) {
+fn html_gen(mut s: String) -> String {
     // entity
-    if code_css.contains("body") {
-        println!("<body>");
+    let entities = vec!["body", "html"];
+    let tag_open_brace = " {";
+    for entity in entities {
+        let entity = entity.to_owned() + tag_open_brace;
+        s = s.replace(&entity, &html_tag_with_open_brace("body"));
     }
+    s
+}
+
+fn html_tag_with_open_brace(s: &str) -> String {
+    format!("<span class=\"tag\">{}</span> {{", s)
+}
+
+// HTML generator for a single tag
+fn html_tag(s: &str) -> String {
+    format!("<span class=\"tag\">{}</span>", s)
 }
 
 /// Check if braces are valid.
@@ -23,15 +38,17 @@ fn valid_braces<'a>(s: &'a str) -> bool {
     let mut stack = Vec::with_capacity(s.len());
 
     for c in s.chars() {
-        if c == '}' {
-            if !stack.is_empty() && stack.last() == Some(&'{') {
-                stack.pop();
-            } else {
-                return false;
+        match c {
+            '}' => {
+                if !stack.is_empty() && stack.last() == Some(&'{') {
+                    stack.pop();
+                } else {
+                    return false;
+                }
             }
-        } else {
             // Add the open brace to the stack.
-            stack.push(c);
+            '{' => stack.push(c),
+            _ => (),
         }
     }
     if stack.is_empty() {
@@ -43,7 +60,6 @@ fn valid_braces<'a>(s: &'a str) -> bool {
 
 #[cfg(test)]
 mod tests {
-
     #[test]
     fn one_open_brace() {
         assert_eq!(super::valid_braces("{"), false);
@@ -72,5 +88,33 @@ mod tests {
     #[test]
     fn two_brace_pairs() {
         assert_eq!(super::valid_braces("{}{}"), true);
+    }
+
+    #[test]
+    fn one_brace_pair_with_word() {
+        assert_eq!(super::valid_braces("{hi}"), true);
+    }
+
+    #[test]
+    fn html_html_tag() {
+        assert_eq!(super::html_tag("html"), "<span class=\"tag\">html</span>");
+    }
+
+    #[test]
+    fn html_body_tag() {
+        assert_eq!(super::html_tag("body"), "<span class=\"tag\">body</span>");
+    }
+
+    #[test]
+    fn html_main_tag() {
+        assert_eq!(super::html_tag("main"), "<span class=\"tag\">main</span>");
+    }
+
+    #[test]
+    fn html_body_tag_with_open_brace() {
+        assert_eq!(
+            super::html_tag_with_open_brace("body"),
+            "<span class=\"tag\">body</span> {"
+        );
     }
 }
